534c572ccba81cdef4a469f69b15d49a43614c95
Skywalerhotel <skywalkerhotel@gmail.com>
2025-04-26 12:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template_string
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
+def preview_website():
+    # Fetch the website content using requests
+    response = requests.get('http://col3neg.com')
+    
+    # Return the raw HTML of the website
+    return render_template_string(response.text)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
